562144a756ebfc434aaadd53d1f2be6a0a424fd9
DEEPA <deepa.cheri@thoughtworks.com>
2020-02-16 22:02:56 +0530

    Updates

---
+++ precache-manifest.71c3d802de058add913b9ca9768ef689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8194eca09fb6caf95bc0da5fb40c8585",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "74be99da62ecf561df40",
+    "url": "/monster-rolodex/static/css/main.cdcbdb91.chunk.css"
+  },
+  {
+    "revision": "dbdcf599bd0019a6ab24",
+    "url": "/monster-rolodex/static/js/2.4fadd9af.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.4fadd9af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74be99da62ecf561df40",
+    "url": "/monster-rolodex/static/js/main.1b5fbf45.chunk.js"
+  },
+  {
+    "revision": "a28171fbd2e5a9b89ffe",
+    "url": "/monster-rolodex/static/js/runtime-main.c8148752.js"
+  }
+]);
